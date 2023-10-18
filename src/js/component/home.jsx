c47d53c8874afa26ab68e344477f6ff147387ed1
Philippe <82619578+phildesignsgfx@users.noreply.github.com>
2023-10-18 18:52:22 +0000

    done23

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,27 +1,39 @@
 import React from "react";
-
+import PropTypes from "prop-types";
 //include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+
 
 //create your first component
 const Home = (props) => {
 	return (
+		<div class="shadow-lg p-3 mb-5 bg-body-tertiary rounded">
 		<div className="text-center mt-5 mx-auto col-2 bg-dark text-light">
 			<div className="row d-flex justify-content-center">
 				<div className="col-2">
-					{/* <i class="fas fa-clock fs-2 text"></i> */}
+				
+				<i class="fas fa-clock fs-1 text"></i>
+					
 				</div>
+				
                 
-				<div className="col-1 fs-1 text">0</div>
-				<div className="col-1 fs-1 text">0</div>
-				<div className="col-1 fs-1 text">0</div>
-				<div className="col-1 fs-1 text">0</div>
-				<div className="col-1 fs-1 text">0</div> 
-				<div className="col-1 fs-1 text">0</div>
+				<div className="col-1 fs-3 text">0</div>
+				<div className="col-1 fs-3 text">0</div>
+				<div className="col-1 fs-3 text">0</div>
+				<div className="col-1 fs-3 text">{props.digit3 % 10}</div>
+				<div className="col-1 fs-3 text">{props.digit2 % 10}</div> 
+				<div className="col-1 fs-3 text">{props.digit1 % 10}</div>
 			</div>
+		</div>
 		</div>
 	);
 	
+	};
+
+Home.propTypes = {
+	digit1: PropTypes.number,
+	digit2: PropTypes.number,
+	digit3: PropTypes.number,
+
 };
 
 
--- src/js/index.js
+++ src/js/index.js
@@ -9,4 +9,16 @@ import "../styles/index.css";
 import Home from "./component/home.jsx";
 
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+let counter = 0;
+setInterval(() => {
+	counter++;
+	let num1 = Math.floor(counter / 1);
+	let num2 = Math.floor(counter / 10);
+	let num3 = Math.floor(counter / 100);
+
+	//render your react application
+	ReactDOM.render(
+		<Home digit1={num1} digit2={num2} digit3={num3} />,
+		document.querySelector("#app")
+	);
+}, 1000);
